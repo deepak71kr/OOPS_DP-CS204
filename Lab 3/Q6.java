4a1477fc2c88763adaa6f0e0163b0b3e91d10281
deepak71kr <93750441+deepak71kr@users.noreply.github.com>
2024-01-30 00:59:55 +0530

    Add files via upload

---
+++ Lab 3/Q6.java
@@ -0,0 +1,30 @@
+public class Lab2Question6 {
+    private void displayValue(int x) {
+        System.out.println("Method with int parameter: " + x);
+    }
+
+    private void displayValue(float x) {
+        System.out.println("Method with float parameter: " + x);
+    }
+
+    private void displayValue(long x) {
+        System.out.println("Method with long parameter: " + x);
+    }
+
+    private void displayValue(String str) {
+        System.out.println("Method with String parameter: " + str);
+    }
+
+    public static void main(String[] args) {
+        TypePromotionDemo demo = new TypePromotionDemo();
+
+        demo.displayValue(10);        
+        demo.displayValue(25.5f);     
+        demo.displayValue(35L);       
+        demo.displayValue("Hello");   
+
+
+    }
+
+    
+}
