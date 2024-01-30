1df3f77ff32c51378c72533e46ce45f39b2ac673
deepak71kr <93750441+deepak71kr@users.noreply.github.com>
2024-01-30 12:25:51 +0530

    Update six.java
    changed return in constructor

--- Lab 4/six.java
+++ Lab 4/six.java
@@ -1,33 +1,34 @@
 package four;
 import java.util.*;
+
 public class six {
     public static class Shape {
         int length;
         int width;
-        private Shape(int length, int width) {
-            this.length = length;
-            this.width = width;
+        public Shape(int... dimns) {
+            if (dimns.length == 1) {
+                this.length = dimns[0];
+                this.width = dimns[0];
+            } else if (dimns.length == 2) {
+                this.length = dimns[0];
+                this.width = dimns[1];
+            }
         }
-        public static Shape createSq(int side) {
-            return new Shape(side, side);
-        }
-        public static Shape createRect(int length, int width) {
-            return new Shape(length, width);
-        }
-        public int calArea() {
+        public int calcArea() {
             return this.length*this.width;
         }
+
         public void display() {
             System.out.println("Length: " + this.length);
             System.out.println("Width: " + this.width);
-            System.out.println("Area: " + calArea());
+            System.out.println("Area: " + calcArea());
         }
     }
     public static void main(String[] args) {
-        Shape sq = Shape.createSq(5);
+        Shape sq = new six.Shape(5);
         sq.display();
 
-        Shape rect = Shape.createRect(5, 10);
+        Shape rect = new six.Shape(5, 10);
         rect.display();
     }
 }
